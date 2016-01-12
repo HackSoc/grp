75d11b820773ed20e845f46b438c4bf8a8d3408d
Michael Walker <mike@barrucadu.co.uk>
2016-01-12 17:41:01 +0000

    Setting GRPDIR in all.sh isn't required

--- all.sh
+++ all.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Install all packages to /tmp/hacksoc-grp
-
-export GRPDIR=${GRPDIR:-/tmp/hacksoc-grp}
-
-if [[ ! -d "$GRPDIR" ]]; then
-    echo "$GRPDIR not found." >&2
-    exit 1
-fi
-
-if [[ ! -d "$GRPDIR/packages" ]]; then
-    echo "$GRPDIR/packages not found." >&2
-    exit 1
-fi
-
 cd packages
 for package in *; do
     echo "Installing $package..."
